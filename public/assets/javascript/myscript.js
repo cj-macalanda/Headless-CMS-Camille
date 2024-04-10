29ba23b6c81143d263ecb81c7855fe44784d230f
Julius0594 <julius.r.mercado@gmail.com>
2024-04-10 12:11:22 -0400

    first commit

---
+++ public/assets/javascript/myscript.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    // Fetch JSON data
+    fetch('../data/content.json')
+        .then(response => response.json())
+        .then(data => {
+
+            // Get the name value from JSON
+            const userProfile = data.userProfile;
+  
+            // Select the <p> element then insert the "name" value beside it
+            const userName = document.querySelector('div .header-icons li p');
+            if (userName) {
+                userName.textContent = userProfile;
+            }
+        })
+        .catch(error => console.error('error', error));
+  });
+
+// =======================================================================
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Fetch JSON data
+    fetch('../data/content.json')
+        .then(response => response.json())
+        .then(data => {
+            // Define an array of card titles
+            const cardTitles = ['Unresolved', 'Overdue', 'Open', 'Hold'];
+            
+            // Iterate over the card titles
+            cardTitles.forEach((title, index) => {
+                // Get the corresponding value from JSON data
+                const value = data[title.toLowerCase()]; // JSON keys are lowercase
+                
+                // Select the corresponding card text element and set its text content
+                const cardTextElement = document.getElementById(`overview-text${index + 1}`);
+                if (cardTextElement) {
+                    cardTextElement.textContent = value;
+                }
+                
+            }        
+        );
+        })
+        .catch(error => console.error('error', error));
+  });
+
+
+  
